2d2acad8c7c0dd3f1b2342bb0e0078e3473e534c
Raghu Betina <raghu@firstdraft.com>
2019-08-09 15:52:46 +0000

    Whitelist columns in Level for ActiveAdmin

--- app/admin/levels.rb
+++ app/admin/levels.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Level do
+
+ permit_params :stage
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
