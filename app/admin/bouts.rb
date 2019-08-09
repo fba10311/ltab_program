693192280865ec1323c91179ee37764124f9a6e5
Raghu Betina <raghu@firstdraft.com>
2019-08-09 15:52:50 +0000

    Whitelist columns in Bout for ActiveAdmin

--- app/admin/bouts.rb
+++ app/admin/bouts.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Bout do
+
+ permit_params :level_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
